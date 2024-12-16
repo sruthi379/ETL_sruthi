ae00bd3354fd35a1e9d455a8418d678bf5cc1c03
Sruthi Kairam <mehersruthi973@gmail.com>
2024-12-16 16:11:42 +0000

    randd

--- models/product_history.sql
+++ models/product_history.sql
@@ -1,52 +1,58 @@
-with
-    batch_data as (
-        select etl_batch_date, etl_batch_no 
-        from metadata.batch_control
+{{ config(
+    materialized='incremental',
+    unique_key='dw_product_id'  
+) }}
+
+WITH 
+    batch_data AS (
+        SELECT etl_batch_date, etl_batch_no 
+        FROM {{source("metadata", "batch_control")}}
     ),
-    update_data as (
-        select 
+    update_data AS (
+        SELECT 
             hist.dw_product_id,
             hist.msrp,
             hist.effective_from_date,
-            0 as dw_active_record_ind,
-            DATEADD(day, -1, b.etl_batch_date) as effective_to_date,
+            0 AS dw_active_record_ind,
+            DATEADD(day, -1, b.etl_batch_date) AS effective_to_date,
             hist.dw_create_timestamp,
-            current_timestamp as dw_update_timestamp,
+            current_timestamp AS dw_update_timestamp,
             hist.create_etl_batch_date,
             hist.create_etl_batch_no,
-            b.etl_batch_no as update_etl_batch_no,
-            b.etl_batch_date as update_etl_batch_date
-        from "dev"."devdw"."product_history" hist
-        join "dev"."devdw"."products" prod 
-            on hist.dw_product_id = prod.dw_product_id
-        cross join batch_data b
-        where 
+            b.etl_batch_no AS update_etl_batch_no,
+            b.etl_batch_date AS update_etl_batch_date
+        FROM {{source("devdw", "product_history")}} hist
+        JOIN {{source("devdw", "products")}} prod 
+            ON hist.dw_product_id = prod.dw_product_id
+        CROSS JOIN batch_data b
+        WHERE 
             hist.msrp != prod.msrp 
-            and hist.dw_active_record_ind = 1
+            AND hist.dw_active_record_ind = 1
     ),
-    insert_data as (
-        select 
+    insert_data AS (
+        SELECT 
             prod.dw_product_id,
             prod.msrp,
-            b.etl_batch_date as effective_from_date,
-            1 as dw_active_record_ind,
-            null as effective_to_date,
-            current_timestamp as dw_create_timestamp,
-            current_timestamp as dw_update_timestamp,
-            b.etl_batch_date as create_etl_batch_date,
-            b.etl_batch_no as create_etl_batch_no,
-            null as update_etl_batch_no,
-            null as update_etl_batch_date
-        from "dev"."devdw"."products" prod
-        left join "dev"."devdw"."product_history" hist
-            on prod.dw_product_id = hist.dw_product_id
-            and hist.dw_active_record_ind = 1
-        cross join batch_data b
-        where hist.dw_product_id is null
+            b.etl_batch_date AS effective_from_date,
+            1 AS dw_active_record_ind,
+            NULL AS effective_to_date,
+            current_timestamp AS dw_create_timestamp,
+            current_timestamp AS dw_update_timestamp,
+            b.etl_batch_date AS create_etl_batch_date,
+            b.etl_batch_no AS create_etl_batch_no,
+            NULL AS update_etl_batch_no,
+            NULL AS update_etl_batch_date
+        FROM {{source("devdw", "products")}} prod
+        LEFT JOIN {{source("devdw", "product_history")}} hist
+            ON prod.dw_product_id = hist.dw_product_id
+            AND hist.dw_active_record_ind = 1
+        CROSS JOIN batch_data b
+        WHERE hist.dw_product_id IS NULL
     ),
-    merged_data as (
-        select * from update_data
-        union all
-        select * from insert_data
+    merged_data AS (
+        SELECT * FROM update_data
+        UNION ALL
+        SELECT * FROM insert_data
     )
-select * from merged_data
+SELECT * 
+FROM merged_data
